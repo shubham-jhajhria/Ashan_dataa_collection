0da8e40fe8b642ef3cfc8a1eca60ec571fc229c9
Prakhar Jaiswal <21229@iiitu.ac.in>
2024-02-20 15:45:29 +0530

    Update csv.kt

--- app/src/main/java/com/shubham/final_project/csv.kt
+++ app/src/main/java/com/shubham/final_project/csv.kt
@@ -1,6 +1,8 @@
 package com.shubham.final_project
 
+import android.os.Build
 import android.os.Environment
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -31,30 +33,75 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
+import com.google.mediapipe.tasks.vision.poselandmarker.PoseLandmarkerResult
 import java.io.File
+import java.time.LocalDateTime
+import java.util.Calendar
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun writeCsv(resultBundle: PoseDetector.ResultBundle){
     val folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
     val csv=File(folder,"${GlobalValues.asanName}.csv")
+    val fileExists = csv.exists()
     csv.createNewFile()
-    csvWriter().open(csv,append = true){
 
+    csvWriter().open(csv,append = true){
+        if (!fileExists) {
+            writeRow(getColumnHeadings())
+        }
         writeRow(appendLandmarkCoordinates(resultBundle))
     }
 }
 
+fun getColumnHeadings(): List<String> {
+    val headings = mutableListOf<String>()
+    // Add column headings for landmarks
+    headings.add("TimeStamp")
+    for (i in 0 until 33) {
+        headings.add("Landmark_${i + 1}_X")
+        headings.add("Landmark_${i + 1}_Y")
+        headings.add("Landmark_${i + 1}_Z")
+        headings.add("Landmark_${i + 1}_Presence")
+        headings.add("Landmark_${i + 1}_Visibility")
+    }
+    // Add additional column headings
+
+    headings.add("Asan_Name")
+    return headings
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
 fun appendLandmarkCoordinates(resultBundle: PoseDetector.ResultBundle): List<String> {
     val coordinatesList: MutableList<String> = mutableListOf()
+    val calendar = Calendar.getInstance()
 
+    val current = LocalDateTime.of(
+        calendar.get(Calendar.YEAR),
+        calendar.get(Calendar.MONTH),
+        calendar.get(Calendar.DAY_OF_MONTH),
+        calendar.get(Calendar.HOUR_OF_DAY),
+        calendar.get(Calendar.MINUTE),
+        calendar.get(Calendar.SECOND)
+    )
+    coordinatesList.add(current.toString())
     resultBundle.results.forEach { result ->
         result.landmarks().forEach { poseLandmarks ->
             poseLandmarks.forEach { landmark ->
-                coordinatesList.add("${landmark.x()},${landmark.y()}")
+                coordinatesList.add("${landmark.x()}")
+                coordinatesList.add("${landmark.y()}")
+                coordinatesList.add("${landmark.z()}")
+                val presence = landmark.presence().toString().removePrefix("Optional[").removeSuffix("]")
+                val visibility = landmark.visibility().toString().removePrefix("Optional[").removeSuffix("]")
+                coordinatesList.add(presence)
+                coordinatesList.add(visibility)
+//                coordinatesList.add("${landmark.presence()}")
+//                coordinatesList.add("${landmark.visibility()}")
             }
         }
     }
+
     coordinatesList.add(GlobalValues.asanName)
     return coordinatesList
 }
